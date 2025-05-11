844e66eb044eee703ba9829f1702d221197428e2
lpodl <justin.pauckert@gmail.com>
2025-05-11 16:02:29 +0200

    load .env relative to take_photo location

--- plantmon/take_photo.sh
+++ plantmon/take_photo.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # load env
-source ../.env
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+source "${SCRIPT_DIR}/../.env"
 # create dir if it doesn't exist
 mkdir -p "${PIC_PATH}"
+
 fswebcam --banner-colour "#40916C" --line-colour "#D8F3DC" -r 1920x1080 --no-shadow --png 0 "${PIC_PATH}/$(date +"%Y-%m-%d_%H%M").png"
